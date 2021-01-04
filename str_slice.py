cdf954ec17cb1d184b95b4dc9d9a1d459a61e9eb
winpy <winpy@gitvsc.com>
2021-01-04 15:52:15 +0530

    String Slicing.

---
+++ str_slice.py
@@ -0,0 +1,28 @@
+data="COMPUTER"
+print("Base String: ",data)
+print("From 2nd Index Number character up to the 5th character.")
+print(data[2:5])
+print("From -6 Index Number character up to the 3rd last character.")
+print(data[-6:-2])
+
+print("Output of [5,2]:",data[5:2])
+print("Output of [-2:-6]:",data[-2:-6])
+
+print("All characters between 3rd and 2nd last character.")
+print(data[3:-2])
+
+print("From 3rd Index Number character up to the last Index Number character.")
+print(data[3:])
+
+print("First 4 characters.")
+print(data[:4])
+
+print("Last 3 characters.")
+print(data[-3:])
+
+print("From 1st character up to the 3rd last character.")
+print(data[:-2])
+
+
+
+
