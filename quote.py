2c86d3eba42559f7a4daa6f0eb36d92edddf8249
winpy <winpy@gitvsc.com>
2021-01-04 15:37:25 +0530

    Use of Quotes and Concatenate String.

---
+++ quote.py
@@ -0,0 +1,7 @@
+#Single Quote inside
+print("Python is World's best Computer Language.")
+#Single Quote and Double Quote inside
+print('"Python" is',"Wolrd's best Computer Language.")
+#Or
+print('''"Python" is Wolrd's best Computer Language.''')
+
---
+++ quote_1.py
@@ -0,0 +1,12 @@
+# String with single quote
+data1='Hello!'
+# String with Double quote
+data2="How are you"
+# String with triple quote
+data3='''Hello! How are you'''
+data4=data1+data2 #Concatenate String
+print(data4)
+print(data1," ",data2)
+print(data3)
+
+
