4e9df6ba00cbe7bc703e19989ff44a886aca321c
winpy <winpy@gitvsc.com>
2021-01-04 15:40:53 +0530

    Use of type() and Conversion.

---
+++ data_type.py
@@ -0,0 +1,9 @@
+a=2
+b="Hello"
+c=10.21
+d=True
+print("Value of a is:",a," and Data type is:",type(a))
+print("Value of b is:",b," and Data type is:",type(b))
+print("Value of c is:",c," and Data type is:",type(c))
+print("Value of d is:",d," and Data type is:",type(d))
+
---
+++ type_conv.py
@@ -0,0 +1,10 @@
+a=10
+b=11
+c_a=str(a)
+c_b=float(b)
+print("Value of a :",a," and Datatype :",type(a))
+print("After conversion Value of a :",c_a," and Datatype :",type(c_a))
+print("Value of a :",b," and Datatype :",type(a))
+print("After conversion Value of b :",c_b," and Datatype :",type(c_b))
+
+
