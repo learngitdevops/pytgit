9a29a77f49965d7aa52e16b0ccc1fe78c392fcce
winpy <winpy@gitvsc.com>
2021-01-08 00:35:27 +0530

    Removing specific element from list using pop().

---
+++ list_pop.py
@@ -0,0 +1,9 @@
+#pop() without argument remove the last element
+#pop(index_number) remove the specific element
+data=[1,2,3,4,5]
+print("Current List :",data)
+print("Removed :",data.pop())
+print("After removing the last element List contains :",data)
+print(data.pop(1)," is at 1st Index and removed.")
+print("Updated List :",data)
+
