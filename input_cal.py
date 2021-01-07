e4d6f20c3606cfdff650768f71f5024b55109481
winpy <winpy@gitvsc.com>
2021-01-07 11:31:57 +0530

    Add, Sub, Mul for 2 numbers using input()

---
+++ input_cal.py
@@ -0,0 +1,10 @@
+#Calculation using 2 values
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+print("The 1st number is ",a," and the 2nd number is ",b)
+add=a+b
+sub=a-b
+mul=a*b
+print("Addition       :",add)
+print("Subtraction    :",sub)
+print("Multiplication :",mul)
