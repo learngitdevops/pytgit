f9a1f6270c61d50522914399f8596eba5f45bd42
winpy <winpy@gitvsc.com>
2021-01-07 12:38:57 +0530

    count() Occurence

---
+++ str_count.py
@@ -0,0 +1,8 @@
+data="Computer Language Python"
+a=data.count("C")
+b=data.count("c")
+c=data.count("t")
+print("Total number of 'C' :",a)
+print("Total number of 'c' :",b)
+print("Total number of 't' :",c)
+
