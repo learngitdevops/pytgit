bcaa784515a500d8f7ecc295cb6ce222eceec03f
winpy <winpy@gitvsc.com>
2021-01-07 21:21:42 +0530

    Accessing Elements of Multidimensional List.

--- create_list.py
+++ create_list.py
@@ -1,5 +1,6 @@
 #List Creation
 #Finding the size of the list
+'''
 list1=[] #blank list
 print("Elements of list1 :",list1)
 print("Number of elements in list1 :",len(list1))
@@ -15,11 +16,14 @@ print("1st Element of list3 at position[0] :",list3[0])
 print("2nd Element of list3 at position[1] :",list3[1])
 print("3rd Element of list3 at position[2] :",list3[2])
 print("Number of elements in list3 :",len(list3))
-
 '''
-list4=[[10,20],["Python","Java"]]
+#Accessing Multidimensional List
+list4=[[10,20,30],["Python","Java","C++"]]
 print("Elements of list4 :",list4)
 print("1st Element of list4 at position[0] :",list4[0])
 print("2nd Element of list4 at position[1] :",list4[1])
 print("Number of elements in list4 :",len(list4))
-'''
+data1=list4[0][0]
+data2=list4[1][2]
+print("1st element of the 1st element :",data1)
+print("3rd element of the 2nd element :",data2)
