afc16db4ccbd0418d1dd73caa31ef790fed5b2b3
winpy <winpy@gitvsc.com>
2021-01-07 23:55:23 +0530

    Removing element which is the 1st occurence from the list.

---
+++ list_remove.py
@@ -0,0 +1,11 @@
+#remove() method remove 1 element at a time
+#remove only the 1st occurence
+data1=["python","java","python","java"]
+data2=[10,20,10,20]
+print("1st List :",data1)
+print("2nd List :",data2)
+data1.remove("python")
+print("1st Updated List :",data1)
+data2.remove(20)
+print("2nd Updated List :",data2)
+
