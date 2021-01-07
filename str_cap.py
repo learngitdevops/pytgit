d32a8355442b54bfabccbb71d252e825ca64d04e
winpy <winpy@gitvsc.com>
2021-01-07 13:43:24 +0530

    str_cap.py file Updated.

--- str_cap.py
+++ str_cap.py
@@ -3,12 +3,15 @@
 data1="computer"
 data2="language"
 data3="python"
+#Concatenate all the 3 strings
 pdata=data1+' '+data2+' '+data3 #putting blank space between string 
-print("Previous String :",pdata)
+#Capitalize the 1st character of each string
 data4=data1.capitalize()
 data5=data2.capitalize()
 data6=data3.capitalize()
+#After using capitalize() Concatenate all the 3 string together
 cdata=data4+' '+data5+' '+data6
+print("Previous String :",pdata)
 print("Current String :",cdata)
 print("------------------------------------------------")
 
