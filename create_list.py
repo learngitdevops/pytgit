ec0d6ad275ef554f92e9eaf2ef7eb40a2f517051
winpy <winpy@gitvsc.com>
2021-01-07 19:28:35 +0530

    Create List, Number of Elements of List, Display the Elements of List

---
+++ create_list.py
@@ -0,0 +1,25 @@
+#List Creation
+#Finding the size of the list
+list1=[] #blank list
+print("Elements of list1 :",list1)
+print("Number of elements in list1 :",len(list1))
+
+list2=[10,20,30]
+print("Elements of list2 :",list2)
+print("Number of elements in list2 :",len(list2))
+
+list3=[10,20,"Python"]
+print("Elements of list3 :",list3)
+print("Position of the Elements in list3.")
+print("1st Element of list3 at position[0] :",list3[0])
+print("2nd Element of list3 at position[1] :",list3[1])
+print("3rd Element of list3 at position[2] :",list3[2])
+print("Number of elements in list3 :",len(list3))
+
+'''
+list4=[[10,20],["Python","Java"]]
+print("Elements of list4 :",list4)
+print("1st Element of list4 at position[0] :",list4[0])
+print("2nd Element of list4 at position[1] :",list4[1])
+print("Number of elements in list4 :",len(list4))
+'''
