a069f74eec698b2726f1feff82a5bd02ffe2af50
winpy <winpy@gitvsc.com>
2021-01-07 15:42:07 +0530

    Replacing Old String with New String using replace().

---
+++ replace.py
@@ -0,0 +1,6 @@
+#Replace Old string with New
+#replace(Old,New)
+odata="'Java' is a computer language."
+print("Previous String :",odata)
+ndata=odata.replace("Java","Python")
+print("After replace New String :",ndata)
