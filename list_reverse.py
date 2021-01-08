000314b505f44e87eff42c5c3a50f0ecdc5ae899
winpy <winpy@gitvsc.com>
2021-01-08 10:50:38 +0530

    Reverse and Sorting List.

---
+++ list_reverse.py
@@ -0,0 +1,4 @@
+data=["python","c++","java","php"]
+print("Current List :",data)
+data.reverse()
+print("In reverse order :",data)
---
+++ list_sort.py
@@ -0,0 +1,8 @@
+data1=["java","python","c++"]
+data2=[20,30,10]
+print("List1 Before Sorting:",data1)
+data1.sort()
+print("After sorting :",data1)
+print("List2 Before Sorting:",data2)
+data2.sort()
+print("After sorting :",data2)
