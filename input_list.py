109dfbfa17568651397d0fe24bf9e84d25ec8e65
winpy <winpy@gitvsc.com>
2021-01-10 18:47:30 +0530

    input_list.py file updated.

--- input_list.py
+++ input_list.py
@@ -1,13 +1,23 @@
 #input() to find out the user specific element from the list
-#Using condition to check the range
-data=[10,"python",30,"java"]
+data=["10","python","30","java"]
 
-rs=str(data)[1:-1] #Converting the List to String
-print("Current items :"+rs)
 
+rs=str(data)[1:-1]     #Converting the List to String
+                       #Remove the 1st and lst character which is [ and ]   
+new=rs.replace("'","") #Remove the single quotes
+print("Current items :",new)
+
+'''
+s="-"
+rs=s.join(data)
+print("Current items :",rs)
+'''
+
+#Taking the input from user and convert that input to integer
 list=input("Enter the position:")
 conv=int(list)
 
+#Using condition to check the range
 if conv==0:
     print("Invalid Choice.")
     print("Please enter a number greater than 0 and smaller than 5.")
